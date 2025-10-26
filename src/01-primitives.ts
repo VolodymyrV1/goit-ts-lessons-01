2b6173d98b8b4c8f9061a4cad545bd76927b2b43
Volodymyr <kotsiubkov@gmail.com>
2025-10-26 21:57:02 +0200

    one

---
+++ src/01-primitives.ts
@@ -0,0 +1,20 @@
+/**
+ * - Прості типи: any, number, string, boolean, null, undefined
+ * - Явна та неявна типізція. Виведення типів
+ */
+
+const test = "test";
+
+console.log(test);
+
+
+let product: string = "lemon";
+
+console.log(product);
+
+// product = 5; // буде помилка
+
+
+// let products = [] //any
+
+
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+
+// import './01-primitives';
+// import './02-objects';
+// import './03-arrays';
+// import './04-types';
+import './05-functions'
+
